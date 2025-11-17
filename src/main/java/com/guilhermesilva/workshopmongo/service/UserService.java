8a77d14855cd352fd57d58c6650cc113a7fabc12
Guilherme Silva <guysilva35x@gmail.com>
2025-11-17 18:28:04 -0300

    busca por ID e tratamento de exceção

--- src/main/java/com/guilhermesilva/workshopmongo/service/UserService.java
+++ src/main/java/com/guilhermesilva/workshopmongo/service/UserService.java
@@ -2,10 +2,12 @@ package com.guilhermesilva.workshopmongo.service;
 
 import com.guilhermesilva.workshopmongo.domain.User;
 import com.guilhermesilva.workshopmongo.repository.UserRepository;
+import com.guilhermesilva.workshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAllUsers(){
         return userRepository.findAll();
+    }
+
+    public User findUserById(String id){
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
     }
 }
